5f0f80aac74fcb7412d18350d1ac13c9969c755d
William <rvw112@gmail.com>
2023-02-22 21:58:07 +1300

    Installing bootstrap and implementing cards for graphs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,110 @@
 <template>
     <main class="home-page">
-        <h1>Home</h1>
-        <p>This is the home page!</p>
+        <!-- <h1>Home</h1>
+        <p>This is the home page!</p> -->
+
+        <div class="row justify-content-center">
+
+            <!-- First section -->
+            <div class="row first-section">
+
+                <div class="card">
+                    <div class="card-header">
+                        <h5>Graph 1</h5>
+                    </div>
+                    <!-- <h2></h2> -->
+                    <div class="card-body">
+                        <!-- <p class="card-text">
+                            Description for this section...
+                        </p> -->
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-header">
+                        <h5>Graph 2</h5>
+                    </div>
+                    <!-- <h2></h2> -->
+                    <div class="card-body">
+                        <!-- <p class="card-text">
+                            Description for this section...
+                        </p> -->
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-header">
+                        <h5>Graph 3</h5>
+                    </div>
+                    <!-- <h2></h2> -->
+                    <div class="card-body">
+                        <!-- <p class="card-text">
+                            Description for this section...
+                        </p> -->
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- Second section -->
+            <div class="row second-section">
+
+                <div class="card">
+                    <div class="card-header">
+                        <h5>Graph 4</h5>
+                    </div>
+                    <!-- <h2></h2> -->
+                    <div class="card-body">
+                        <!-- <p class="card-text">
+                            Description for this section...
+                        </p> -->
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-header">
+                        <h5>Graph 5</h5>
+                    </div>
+                    <h2></h2>
+                    <div class="card-body">
+                        <!-- <p class="card-text">
+                            Description for this section...
+                        </p> -->
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+        
+
     </main>
 </template>
+
+<style lang="scss" scoped>
+    .row {
+        height: 100%;
+
+        .first-section {
+            width: 18rem;
+            margin: 0 1rem;
+
+            .card {
+                margin: 1rem 0;
+                padding: 0;
+            }
+        }
+
+        .second-section {
+        width: 30rem;
+        margin: 0 1rem;
+
+        .card {
+            margin: 1rem 0;
+            padding: 0;
+        }
+    }
+}
+
+</style>
